b46860eb97a50ca6f3084aed8d57c69a98c6431b
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-01-02 22:01:58 +0000

    Fixed age

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
-    // Set my age
-    bday = new Date("2001-09-28");
-    now = new Date();
-    document.getElementById("age").innerText = now.getFullYear() - bday.getFullYear();
+    birthDate = new Date("2001-09-28");
+	difference = Date.now() - birthDate.getTime();
+	ageDate = new Date(difference); 
+	calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
+    document.getElementById("age").innerText = calculatedAge;
 };
