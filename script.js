426246781c20ec0132410050b4b7aac3a87b218d
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-03-18 11:25:21 +0000

    Update script.js
    JavaScript is now WTFPL licenced, for LibreJS

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// @license magnet:?xt=urn:btih:723febf9f6185544f57f0660a41489c7d6b4931b&dn=wtfpl.txt WTFPL
 window.onload = function() {
     birthDate = new Date("2001-09-28");
-	difference = Date.now() - birthDate.getTime();
-	ageDate = new Date(difference); 
-	calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
+    difference = Date.now() - birthDate.getTime();
+    ageDate = new Date(difference); 
+    calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
     document.getElementById("age").innerText = calculatedAge;
 };
+// @license-end
